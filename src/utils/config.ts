5d19c1a44e6f7ee83f58ae8a0f7aa4a2f34e6fb1
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2022-06-30 12:16:11 -0500

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,3 +5,11 @@ export const DB_USER = "";
 export const DB_PASSWORD = "";
 export const DB_NAME = "";
 export const DB_PORT = 3306;
+
+export const DB_CONFIG = {
+	host: DB_SERVICE_URL,
+	user: DB_USER,
+	password: DB_PASSWORD,
+	database: DB_NAME,
+	port: DB_PORT,
+};
