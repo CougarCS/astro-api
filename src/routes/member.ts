aa6207acea81a69d13896dd564c85e3c14292625
Anil-Shanker <anilshanker2@gmail.com>
2022-07-05 22:13:58 -0500

    Implemented member ledger file endpoint

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { query, validationResult } from "express-validator";
+import { body, query, validationResult } from "express-validator";
+import * as path from "path";
 
 import MemberService from "../services/member-service";
 
@@ -28,6 +29,34 @@ router.get(
 			return res.status(200).json({ membership });
 		} catch (err) {
 			logger.error("MemberService.isMember failed. Error =");
+			logger.error(err);
+		}
+
+		return res.status(500).json({ message: "Unable to load resource" });
+	}
+);
+
+/* POST /member/ledger */
+
+router.post(
+	"/ledger",
+	body("fields").isArray().notEmpty(),
+	body("headers").isArray().optional(),
+	body("fields.*").isString().notEmpty(),
+	body("headers.*").isString().optional(),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { fields, headers } = req.body;
+
+		try {
+			await MemberService.writeLedgerFile(fields, headers);
+			return res.status(200).download(path.resolve(__dirname, "../services/tmp.csv"));
+		} catch (err) {
+			logger.error("MemberService.getLedgerFile failed. Error =");
 			logger.error(err);
 		}
 
--- src/services/member-service.ts
+++ src/services/member-service.ts
@@ -1,5 +1,6 @@
-// import { type } from "os";
+import { writeToPath } from "fast-csv";
 import logger from "../utils/logger/logger";
+import * as path from "path";
 
 import SQLService from "./sql-service";
 
@@ -23,6 +24,17 @@ class MemberService {
 			start_date: result[0].start_date,
 			end_date: result[0].end_date,
 		};
+	}
+
+	static async writeLedgerFile(fields: string[], headers: string[]) {
+		logger.info("MemberService.getLedgerFile invoked! Fields = " + fields);
+		
+		const ledger_data = await SQLService.select("contact", { fields: fields });
+		const rows = ledger_data.map(Object.values);
+
+		writeToPath(path.resolve(__dirname, "tmp.csv"), rows, { headers: headers })
+			.on("error", (err) => console.error(err))
+			.on("finish", () => console.log("Done writing."));
 	}
 }
 
