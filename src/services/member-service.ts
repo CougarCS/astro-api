1e5fe01db6107e62bc6692cec4cc6d6faf8f83c9
Ben Key <54153434+mellophone@users.noreply.github.com>
2022-07-11 22:42:50 -0500

    redirected member-service.ts to the active_member view

--- src/services/member-service.ts
+++ src/services/member-service.ts
@@ -6,22 +6,23 @@ import SQLService from "./sql-service";
 class MemberService {
 	static async isMember(uh_id = "", email = "") {
 		logger.info(`uh_id=${uh_id} email=${email}`);
+
 		const constraints = [];
 		if (email !== "") constraints.push({ field: "email", value: email });
 		if (uh_id !== "") constraints.push({ field: "uh_id", value: uh_id });
-		const result = await SQLService.select("member", {
-			fields: ["start_date", "end_date"],
+
+		const result = await SQLService.select("active_member", {
+			fields: ["start_date", "end_date", "member_points"],
 			constraints,
 			compare: "AND",
 		});
-		let status = false;
-		if (result.length === 0) return { status };
-		if (result[0].start_date < new Date() && new Date() < result[0].end_date)
-			status = true;
+		
+		if (result.length === 0) return { status: false };
 		return {
-			status,
+			status: true,
 			start_date: result[0].start_date,
 			end_date: result[0].end_date,
+			member_points: result[0].member_points
 		};
 	}
 }
