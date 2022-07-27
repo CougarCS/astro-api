7ba6882a9a40e8cf4a4008cfb321bdc6b5557483
John <44646589+Junnyyy@users.noreply.github.com>
2022-07-26 19:28:43 -0500

    ✨ Added createMember()

--- src/services/member-service.ts
+++ src/services/member-service.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
 
 import logger from "../utils/logger/logger";
 
@@ -39,6 +40,30 @@ class MemberService {
 			start_date: membership.start_date,
 			end_date: membership.end_date,
 		};
+	}
+
+	static async createMember(
+		contact_id: string,
+		start_date: Date,
+		end_date: Date,
+		membership_code: string
+	) {
+		logger.info(
+			`MemberService.createMember invoked! contact_id=${contact_id} start_date=${start_date} end_date=${end_date} membership_code:${membership_code}`
+		);
+
+		const prisma = new PrismaClient();
+		const UUID = uuidv4();
+
+		await prisma.membership.create({
+			data: {
+				membership_id: UUID,
+				contact_id: contact_id,
+				start_date: start_date,
+				end_date: end_date,
+				membership_code_id: membership_code,
+			},
+		});
 	}
 }
 
