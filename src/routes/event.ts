a454353e49490b0f684c339af413490fb30fdb2e
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-08-22 08:43:29 -0500

    ✨ Added POST event

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 import EventService from "../services/event-service";
+
+import logger from "../utils/logger/logger";
 
 const router = Router();
 
@@ -8,9 +11,38 @@ const router = Router();
 
 // Creates an event
 
-router.post("/", (req, res) => {
-	res.status(200).json({ message: "Project ASTRO API 🚀" });
-});
+router.post(
+	"/",
+	body("title").isString().notEmpty(),
+	body("description").isString().notEmpty(),
+	body("datetime").isString().notEmpty(),
+	body("duration").isFloat().notEmpty(),
+	body("point_value").isInt().notEmpty(),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, description, datetime, duration, point_value } = req.body;
+
+		try {
+			const event = await EventService.createEvent(
+				title,
+				description,
+				datetime,
+				duration,
+				point_value
+			);
+			return res.status(200).json({ event });
+		} catch (err) {
+			logger.error("EventService.createEvent failed. Error=");
+			logger.error(err);
+		}
+
+		res.status(500).json({ message: "Unable to load resource" });
+	}
+);
 
 /* POST /event/attendance */
 
--- src/services/event-service.ts
+++ src/services/event-service.ts
@@ -1,8 +1,36 @@
+import { v4 as uuidv4 } from "uuid";
+
 import { prisma } from "../utils/prisma";
+import logger from "../utils/logger/logger";
 
 class EventService {
-	static async createEvent() {
-		console.log("createEvent");
+	static async createEvent(
+		title: string,
+		description: string,
+		datetime: string,
+		duration: number,
+		point_value: number
+	) {
+		logger.info(
+			`EventService.createEvent invoked! title=${title} description=${description} date=${datetime} duration=${duration} point_value=${point_value} `
+		);
+
+		const UUID = uuidv4();
+
+		const formattedDatetime = new Date(datetime);
+
+		const event = prisma.event.create({
+			data: {
+				event_id: UUID,
+				title: title,
+				description: description,
+				datetime: formattedDatetime,
+				duration: duration,
+				point_value: point_value,
+			},
+		});
+
+		return event;
 	}
 
 	static async addAttendance() {
